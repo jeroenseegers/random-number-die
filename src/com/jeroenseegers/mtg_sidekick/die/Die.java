d61d2edbaeea376413877046c27ad9ba2da706df
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2014-05-10 16:46:32 +0200

    Add Die class

---
+++ src/com/jeroenseegers/mtg_sidekick/die/Die.java
@@ -0,0 +1,22 @@
+package com.jeroenseegers.mtg_sidekick.die;
+
+public class Die {
+
+	private int mMinValue;
+	private int mMaxValue;
+	private int mCurrentValue;
+
+	public Die(int minValue, int maxValue) {
+		this.mMinValue = minValue;
+		this.mMaxValue = maxValue;
+	}
+
+	public void rollDie() {
+		this.mCurrentValue = (int)(Math.random() * this.mMaxValue) + this.mMinValue;
+	}
+
+	public int getCurrentValue() {
+		return this.mCurrentValue;
+	}
+
+}
