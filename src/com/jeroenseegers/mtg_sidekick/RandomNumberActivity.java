4f3f397a2702503d5c795845ec6180effabcc5c6
Jeroen Seegers <jeroen.seegers+github@gmail.com>
2014-05-10 16:08:37 +0200

    Added onclick to button

--- src/com/jeroenseegers/mtg_sidekick/RandomNumberActivity.java
+++ src/com/jeroenseegers/mtg_sidekick/RandomNumberActivity.java
@@ -2,13 +2,26 @@ package com.jeroenseegers.mtg_sidekick;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class RandomNumberActivity extends Activity {
+public class RandomNumberActivity extends Activity implements OnClickListener {
 
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random_number);
+
+        Button randomNumberButton = (Button)findViewById(R.id.randomNumberButton);
+
+        randomNumberButton.setOnClickListener(this);
     }
+	
+	@Override
+	public void onClick(View arg0) {
+		Log.d("mtg_sidekick", "Button clicked");
+	}
 
 }
